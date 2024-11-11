d9f0606d99fb5c3de514fa1bc031fabad68631e5
apurbo_ibn <114634353+apurboibn@users.noreply.github.com>
2024-11-11 13:56:38 +0600

    Update task7.php

--- Lab3/task7.php
+++ Lab3/task7.php
@@ -1,25 +1,28 @@
-
-
 <?php
+print("<html><table border = '1' width='200' cellspacing='0'> <tr> ");
+print("<td>");
 for ($i = 1; $i <= 3; $i++) {
     for ($j = 1; $j <= $i; $j++) {
         echo "* ";
     }
     echo "<br>";
 }
-?>
 
-<?php
+print("</td>");
+echo"<br>";
+
+print("<td>");
 for ($i = 3; $i >= 1; $i--) {
     for ($j = 1; $j <= $i; $j++) {
         echo $j . " ";
     }
     echo "<br>";
 }
-?>
 
 
-<?php
+print("</td>");
+echo"<br>";
+print("<td>");
 $word = 'A';
 for ($i = 1; $i <= 3; $i++) {
     for ($j = 1; $j <= $i; $j++) {
@@ -28,4 +31,7 @@ for ($i = 1; $i <= 3; $i++) {
     }
     echo "<br>";
 }
+print("</td>");
+echo"<br>";
+print("</tr></table></html>")
 ?>
