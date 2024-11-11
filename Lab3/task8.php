727db1e8c3968a5773aa08210fcfe07103479dd8
apurbo_ibn <114634353+apurboibn@users.noreply.github.com>
2024-11-11 13:58:07 +0600

    task8.php

--- Lab3/task8.php
+++ Lab3/task8.php
@@ -1,25 +1,31 @@
 <?php
+
 $array = [
     [1, 2, 3, 'A'],
     [1, 2, 'B', 'C'],
     [1, 'D', 'E', 'F']
 ];
-?>
-
-<?php
+print("<html><table border = '1' width='200' cellspacing='0'> <tr> ");
+print("<td>");
 for ($i = 0; $i < 3; $i++) {
     for ($j = 0; $j < 3 - $i; $j++) {
         echo $array[$i][$j] . " ";
     }
     echo "<br>";
 }
-?>
+print("</td>");
+echo"<br>";
+print("<td>");
 
-<?php
+
 for ($i = 0; $i < 3; $i++) {
     for ($j = 3 - $i; $j < 4; $j++) {
         echo $array[$i][$j] . " ";
     }
     echo "<br>";
 }
+
+print("</td>");
+echo"<br>";
+print("</tr></table></html>")
 ?>
