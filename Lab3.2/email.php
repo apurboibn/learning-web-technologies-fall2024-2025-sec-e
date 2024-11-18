b2b95efd2fe2d51172d01b4439bfde0d21fbca9b
apurbo_ibn <114634353+apurboibn@users.noreply.github.com>
2024-11-18 14:22:42 +0600

    email.php

--- Lab3.2/email.php
+++ Lab3.2/email.php
@@ -5,19 +5,15 @@ if (isset($_POST['email_submit']))
 
     if (empty($email)) 
     {
-       echo  "Email cannot be empty.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+        echo "Email cannot be empty.";
+    } 
+    elseif (strpos($email, '@') === false || strpos($email, '.') === false || strpos($email, '@') > strrpos($email, '.')) 
     {
-       echo  "Invalid email format.";
-    }
-
-    if (empty($errors)) 
+        echo "Invalid email format.";
+    } 
+    else 
     {
         echo "Email is valid.";
-    } else {
-        foreach ($errors as $error) {
-            echo "<p>$error</p>";
-        }
     }
 }
 ?>
